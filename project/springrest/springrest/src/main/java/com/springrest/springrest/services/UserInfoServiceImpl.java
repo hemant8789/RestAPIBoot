c5c00621d8c078b16293753b1e39e7c07228edee
hemant8789 <90678348+hemant8789@users.noreply.github.com>
2023-04-17 18:08:28 +0530

    Update UserInfoServiceImpl.java

--- project/springrest/springrest/src/main/java/com/springrest/springrest/services/UserInfoServiceImpl.java
+++ project/springrest/springrest/src/main/java/com/springrest/springrest/services/UserInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.springrest.springrest.services;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,11 @@ import javax.management.openmbean.KeyAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.Exception.KeyAlreadyExists;
 import com.springrest.springrest.dao.UserInfoDao;
 import com.springrest.springrest.entites.Userprofile;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service 
@@ -24,7 +28,6 @@ public class UserInfoServiceImpl implements UserInfoService{
 	@Override
 	public List<Userprofile> getUserInfo() {
 	
-		
 		return userInfoDao.findAll() ;
 	}
 
@@ -42,29 +45,33 @@ public class UserInfoServiceImpl implements UserInfoService{
 	public Userprofile addUserprofile(Userprofile profile) {
 		
 //		list.add(profile);
-		try {			
+					
 			List<Userprofile> allusers= userInfoDao.findAll();
 			allusers.forEach(user -> {
 				if(user.getEmailId().equals (profile.getEmailId())) {
-					throw new KeyAlreadyExistsException("duplicate email");
+					throw new KeyAlreadyExists("duplicate email");
 				}
 			});
 			System.out.println(profile.getRole());
-			profile.getRole().setUserprofile(profile);
 			
-			
-			userInfoDao.save(profile);
-		} catch (KeyAlreadyExistsException e) {
-			
-			throw new KeyAlreadyExistsException(e.getMessage());
-		}
+		
+		userInfoDao.save(profile);
 		return profile;
 	}
 	
 	@Override
-	public Userprofile updateUserprofile(Userprofile profile) {
-		
-		userInfoDao.save(profile);
+	public Userprofile updateUserprofile(long userId, Userprofile profile) throws EntityNotFoundException {
+		try {			
+			boolean exist = userInfoDao.existsById(userId);
+			if(!exist) {
+				throw new EntityNotFoundException("no such user found");
+			}
+			profile.setUserId(userId);
+			userInfoDao.save(profile);
+		} catch (EntityNotFoundException e) {
+			// TODO: handle exception
+			throw new EntityNotFoundException(e.getMessage());
+		}
 		return profile;
 		
 	}
@@ -82,9 +89,9 @@ public class UserInfoServiceImpl implements UserInfoService{
 	@Override
 	public Optional<Userprofile> getUserprofile(long UserId) {
 		
-		return userInfoDao.findById(UserId);
+		return userInfoDao.findById(UserId); 
 	}
 
-		
+	//optional so that the client can check if profile exist or not.	
 
 }
