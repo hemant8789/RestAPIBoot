a64098988a68f5fbf610f9e597b58ab2d7cc8e0e
hemant8789 <90678348+hemant8789@users.noreply.github.com>
2023-04-17 18:05:00 +0530

    Update Userprofile.java

--- project/springrest/springrest/src/main/java/com/springrest/springrest/entites/Userprofile.java
+++ project/springrest/springrest/src/main/java/com/springrest/springrest/entites/Userprofile.java
@@ -1,8 +1,5 @@
 package com.springrest.springrest.entites;
 
-import org.hibernate.validator.constraints.UniqueElements;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.annotation.Generated;
@@ -22,134 +19,151 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-
 @Entity
 public class Userprofile {
 	
-	@Id	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long UserRoleId;
+	private long userId;
 	
 	@NotEmpty
-	@Size(min = 4, message = "INVALID INPUT!!") 
-	private String Name;
+	@Size(min = 4, message = " Name field Should not be Empty!!")
+	private String name;
 	
-	@NotEmpty(message = "FORMAT ERROR!!")
-	private String DOB;
+	@NotEmpty(message = "Please enter DOB")
+	private String dob;
 	
 	@NotEmpty(message = "It should not be empty!!")
-	private String Education;
+	private String education;
 	
-//	@NotEmpty(message = "Marital status should not be empty!!")
-	@NotBlank(message = "Should not be empty")
+	@NotBlank(message = "should not be empty!!")
 	private String martialStatus;
 	
 	@NotNull
 	private long phoneNumber;
 	
-//	@UniqueEmail(message = "Duplicate EmailId")
+
 	@Email(message = "FORMAT ERROR!!")
-	@NotEmpty(message = "should not be empty")
-	@Column(unique = true)
+	@NotEmpty(message = "Should not be empty!!")
+	@Column
 	private String emailId;
+
 	
-	
-	@OneToOne(mappedBy = "userprofile", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
 	private Role role;
 
-	public Userprofile(long userRoleId, @NotEmpty @Size(min = 4, message = "INVALID INPUT!!") String name,
-			@NotEmpty(message = "FORMAT ERROR!!") String dOB,
+
+	public Userprofile(long userId, @NotEmpty @Size(min = 4, message = "INVALID INPUT!!") String name,
+			@NotEmpty(message = "FORMAT ERROR!!") String dob,
 			@NotEmpty(message = "It should not be empty!!") String education,
-			@NotBlank(message = "Should not be empty") String martialStatus, @NotNull long phoneNumber,
-			@Email(message = "FORMAT ERROR!!") @NotEmpty(message = "should not be empty") String emailId, Role role) {
+			@NotBlank(message = "should not be empty!!") String martialStatus, @NotNull long phoneNumber,
+			@Email(message = "FORMAT ERROR!!") @NotEmpty(message = "Should not be empty!!") String emailId, Role role) {
 		super();
-		this.UserRoleId = userRoleId;
-		this.Name = name;
-		this.DOB = dOB;
-		this.Education = education;
+		this.userId = userId;
+		this.name = name;
+		this.dob = dob;
+		this.education = education;
 		this.martialStatus = martialStatus;
 		this.phoneNumber = phoneNumber;
 		this.emailId = emailId;
 		this.role = role;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Userprofile [UserRoleId=" + UserRoleId + ", Name=" + Name + ", DOB=" + DOB + ", Education=" + Education
+		return "Userprofile [userId=" + userId + ", name=" + name + ", dob=" + dob + ", education=" + education
 				+ ", martialStatus=" + martialStatus + ", phoneNumber=" + phoneNumber + ", emailId=" + emailId
 				+ ", role=" + role + "]";
 	}
 
-	public long getUserRoleId() {
-		return UserRoleId;
+	public Userprofile() {
+		super();
+		
 	}
 
-	public void setUserRoleId(long userRoleId) {
-		UserRoleId = userRoleId;
+
+	public Userprofile(long userId2, String name2, String dob2, String education2, String martialStatus2,
+			long phoneNumber2, String emailId2, String string) {
+		// TODO Auto-generated constructor stub
 	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
 
 	public String getName() {
-		return Name;
+		return name != null ? name : "";
 	}
+
 
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
 
-	public String getDOB() {
-		return DOB;
+
+	public String getDob() {
+		return dob;
 	}
 
-	public void setDOB(String dOB) {
-		DOB = dOB;
+
+	public void setDob(String dob) {
+		this.dob = dob;
 	}
+
 
 	public String getEducation() {
-		return Education;
+		return education;
 	}
 
+
 	public void setEducation(String education) {
-		Education = education;
+		this.education = education;
 	}
+
 
 	public String getMartialStatus() {
 		return martialStatus;
 	}
 
+
 	public void setMartialStatus(String martialStatus) {
 		this.martialStatus = martialStatus;
 	}
+
 
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
 
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 
 	public String getEmailId() {
 		return emailId;
 	}
 
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 
 	public Role getRole() {
 		return role;
 	}
 
+
 	public void setRole(Role role) {
 		this.role = role;
 	}
-
-	public Userprofile() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	
 
 }
