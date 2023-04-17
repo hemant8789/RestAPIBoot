4da065fa43b92b052b7b83a11388411167137b39
hemant8789 <90678348+hemant8789@users.noreply.github.com>
2023-04-17 18:08:05 +0530

    Update UserInfoService.java

--- project/springrest/springrest/src/main/java/com/springrest/springrest/services/UserInfoService.java
+++ project/springrest/springrest/src/main/java/com/springrest/springrest/services/UserInfoService.java
@@ -1,24 +1,25 @@
 package com.springrest.springrest.services;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 import com.springrest.springrest.entites.Userprofile;
 
-public interface UserInfoService {
-	
-	public List<Userprofile> getUserInfo();
-	
-	public Optional<Userprofile> getUserprofile(long UserId);
-	
-	public Userprofile addUserprofile(Userprofile profile);
-	
-	public Userprofile updateUserprofile(Userprofile profile);
-	
-//	public void deleteUserprofile(Integer parseInteger);
+import jakarta.persistence.EntityNotFoundException;
 
+public interface UserInfoService {//defines the method that need to be implemented by the classes
+	
+	public List<Userprofile> getUserInfo();//provide list of user profile
+	
+	public Optional<Userprofile> getUserprofile(long UserId);//used to retrieve single user profile
+	
+	public Userprofile addUserprofile(Userprofile profile);//add new user profile to database
+	
+	public Userprofile updateUserprofile(long userId, Userprofile profile) throws EntityNotFoundException;
+	//update an existing user profile
 	Userprofile getUserprofile(Long UserId);
 
-	public void deleteUserprofile(Long UserId);
+	public void deleteUserprofile(Long UserId);//delete a user profile with given user id.
 	
 }
