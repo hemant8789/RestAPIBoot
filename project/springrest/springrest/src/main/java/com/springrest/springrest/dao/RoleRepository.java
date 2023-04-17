eee4a31a2c8ee6c7fc89ff8bde21055e58eb7ebc
hemant8789 <90678348+hemant8789@users.noreply.github.com>
2023-04-17 18:03:16 +0530

    Update RoleRepository.java

--- project/springrest/springrest/src/main/java/com/springrest/springrest/dao/RoleRepository.java
+++ project/springrest/springrest/src/main/java/com/springrest/springrest/dao/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.springrest.springrest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springrest.springrest.entites.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
